0c2eea8350208383aee5de11496e4f00d554e9dc
AO-AO <xuao@awcloud.com>
2016-02-17 13:21:35 +0800

    exam2
    Write a program that maps a list of words into a list of integers representing the lengths of the correponding words.

--- exam2.py
+++ exam2.py
@@ -1,7 +1,7 @@
 def trans_words2count(word_list):
     for index in xrange(len(word_list)):
         if type(word_list[index]) != str:
-            print("An item is not words!")
+            print("Items must be words!")
             return 0
         else:
             word_list[index] = len(word_list[index])
